4e93c460fd22feaf2d7f516acdaaaf706bd08a54
graspconcepts <graspconcepts@users.noreply.github.com>
2021-03-17 06:05:11 -0500

    Update Thumbnail.cs

--- ImageFunctions/Thumbnail.cs
+++ ImageFunctions/Thumbnail.cs
@@ -63,6 +63,7 @@ namespace ImageFunctions
                         encoder = new GifEncoder();
                         break;
                     default:
+                        encoder = new PngEncoder();
                         break;
                 }
             }
